c0fc92c6536fb4711f57d61b2a44d638094e69cf
Nele Müller <nm@fp.de>
2018-03-22 15:29:35 +0100

    Refresh gitignore file

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Nele on 17.03.18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any   after loading the view, typically from a nib.
+    }
+
+}
+
